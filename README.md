c70985a89b1f97501657493212e4139429bf78f6
freewind <nowindlee@gmail.com>
2018-07-29 00:14:20 +0800

    done, hard work

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 Java Mustache Demo
 ==================
 
-How to render a simple mustache template in Java.
+How to render a mustache template which inherit from another layout template in Java.
 
 Run `Hello.java` in your IDE.
+
+Comments
+--------
+
+Can't understand why a project which has been developed for many years and have many users, won't give good examples to show the features it has?
+
+I spent several hours to make this demo work, all the information the official README gives only:
+
+> Template inheritance is supported by this implementation, see https://github.com/mustache/spec/issues/38 (eg. {{<super}}{{$content}}...{{/content}}{{/super}})
+
+It's not easy to GUESS the correct usage for a person who is not a pro of mustache.
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -6,19 +6,11 @@ import com.github.mustachejava.MustacheFactory;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
-
-import static com.github.freewind.lostlist.Lists.list;
 
 public class Hello {
 
     public static void main(String[] args) throws IOException {
-        Page page = new Page(
-                list("aaa@test.com", "bbb@test.com"),
-                list(
-                        new Item("Item 1", "$19.99", list(new Feature("New!"), new Feature("Awesome!"))),
-                        new Item("Item 2", "$29.99", list(new Feature("Old."), new Feature("Ugly.")))
-                ));
+        Page page = new Page("Mustache");
         MustacheFactory factory = new DefaultMustacheFactory("templates");
 
         Mustache mustache = factory.compile(factory.getReader("hello.html"), "hello");
@@ -28,28 +20,8 @@ public class Hello {
 }
 
 class Page {
-    final List<String> emails;
-    final List<Item> items;
-    public Page(List<String> emails, List<Item> items) {
-        this.emails = emails;
-        this.items = items;
-    }
-}
-
-class Feature {
-    final String description;
-    public Feature(String description) {
-        this.description = description;
-    }
-}
-
-class Item {
     final String name;
-    final String price;
-    final List<Feature> features;
-    public Item(String name, String price, List<Feature> features) {
+    public Page(String name) {
         this.name = name;
-        this.price = price;
-        this.features = features;
     }
 }
